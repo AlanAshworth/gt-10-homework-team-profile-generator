f6916fa4ed31b9c2fc0d3ad68595e085a87b7944
Alan Ashworth <awashworth927@gmail.com>
2020-03-31 21:56:56 -0400

    Flesh out readme. Create gif for readme. Tweak class files for consistency. Remove instructions. Modify app.js for re-usable code.

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # GT-Homework-10-Team-Profile-Generator
+
+A Node CLI that takes in information about employees and generates an HTML webpage that displays summaries for each person.
+
+## Description
+
 The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Tests](#tests)
+* [Contributing](#contributing)
+* [Repository](#repository)
+* [Questions](#questions)
+* [License](#license)
+
+## Installation
+
+Install the necessary dependencies in the command line with:
+
+```sh
+npm install
+```
+
+## Usage
+
+After dependencies are installed, run the application in the command line with:
+
+```sh
+node app.js
+```
+
+The following demonstrates application functionality:
+
+![team-profile generator demo](./Assets/team-profile-generator.gif)
+
+## Tests
+
+To run tests, please run the following command:
+
+```
+npm run test
+```
+
+## Contributing
+
+None.
+
+## Repository
+
+[Github Repository](https://github.com/AlanAshworth/GT-Homework-10-Team-Profile-Generator)
+
+## Questions
+
+<img src="https://avatars3.githubusercontent.com/u/54105679?v=4" alt="avatar" width="50px" height="50px" />
+
+Contact me at <a href="mailto:awashworth927@gmail.com">awashworth927@gmail.com</a>
+
+## License
+
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- app.js
+++ app.js
@@ -8,129 +8,107 @@ const render = require("./lib/htmlRenderer");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 var team = [];
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-buildTeam();
+var hasManager = false;
 
-function buildTeam() {
-  getManager();
-  getTeam();
-}
-
-function getManager() {
-  return inquirer
-    .prompt([
-      {
-        name: "role",
-        type: "input",
-        message: "Plase add a team manager. What is the manager's name?"
-      },
-      {
-        name: "id",
-        type: "input",
-        message: "What is the manager's ID?"
-      },
-      {
-        name: "email",
-        type: "input",
-        message: "What is the manager's email address?"
-      },
-      {
-        name: "officeNumber",
-        type: "input",
-        message: "What is the manager's office number?"
-      }
-    ])
-    .then(data => {
-      team.push(new Manager(data.name, data.id, data.email, data.officeNumber));
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-function getTeam() {
+const getTeam = () => {
   return inquirer
     .prompt([
       {
         name: "role",
         type: "list",
-        message: "Plase add a team role. What role would you like to add?",
-        choices: ["Engineer", "Intern"]
+        message: "Enter the person's role:",
+        choices: () => {
+          if (!hasManager) {
+            hasManager = !hasManager;
+            return ["Manager"];
+          } else {
+            return ["Engineer", "Intern"];
+          }
+        }
+      },
+      {
+        name: "name",
+        type: "input",
+        message: "Enter the person's name:"
       },
       {
         name: "id",
         type: "input",
-        message: "What is the team member's ID?"
+        message: "Enter the person's ID:"
       },
       {
         name: "email",
         type: "input",
-        message: "What is the team member's email address?"
+        message: "Enter the person's email address:"
+      },
+      {
+        name: "officeNumber",
+        type: "input",
+        message: "Enter the person's office number:",
+        when: data => data.role === "Manager"
       },
       {
         name: "github",
         type: "input",
-        message: "What is the engineer's github username?",
+        message: "Enter the person's github username:",
         when: data => data.role === "Engineer"
       },
       {
         name: "school",
         type: "input",
-        message: "What is the interns's school?",
+        message: "Enter the person's school:",
         when: data => data.role === "Intern"
       }
     ])
     .then(data => {
-      if (data.role === "Engineer") {
-        team.push(new Engineer(data.name, data.id, data.email, data.github));
-      } else {
-        team.push(new Intern(data.name, data.id, data.email, data.school));
+      switch (data.role) {
+        case "Manager":
+          team.push(new Manager(data.name, data.id, data.email, data.officeNumber));
+          break;
+        case "Engineer":
+          team.push(new Engineer(data.name, data.id, data.email, data.github));
+          break;
+        case "Intern":
+          team.push(new Intern(data.name, data.id, data.email, data.school));
+          break;
+        default:
+          throw new Error("Invalid selection");
       }
-      addMember();
+      getMember();
     })
     .catch(error => {
       console.log(error);
     });
-}
+};
 
-function addMember() {
-  inquirer
+const getMember = () => {
+  return inquirer
     .prompt([
       {
-        name: "addMember",
-        type: "list",
-        message: "Would you like to add another member?",
-        choices: ["yes", "no"]
+        name: "select",
+        type: "confirm",
+        message: "Enter another person?"
       }
     ])
     .then(data => {
-      if (data.addMember === "yes") {
-        this.getTeam();
+      if (data.select) {
+        getTeam();
       } else {
-        fs.writeFile(outputPath, render(team), error => {
-          if (error) {
-            throw error;
-          } else {
-            console.log("Successfully created ", outputPath);
-          }
-        });
+        writeToHTML();
       }
     });
-}
+};
+
+const writeToHTML = () => {
+  var directory = "./output";
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+
+  fs.writeFile(outputPath, render(team), error => {
+    if (error) throw error;
+    console.log("Created successfully");
+  });
+};
+
+getTeam();
