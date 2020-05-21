0d50f90508832ff56d31c29da97a05f515267065
Alan Ashworth <awashworth927@gmail.com>
2020-05-21 00:11:52 -0400

    Add some cool ascii art for CLI startup.

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+const logo = require("asciiart-logo");
+const config = require("./package.json");
 const TeamBuilder = require("./lib/TeamBuilder");
 
-const teamBuilder = new TeamBuilder();
+const longText =
+  "A Node CLI that takes in information about employees and generates an HTML webpage that displays summaries for each person.";
 
+console.log(
+  logo({
+    name: "Team Profile Generator",
+    font: "Small Slant",
+    lineChars: 10,
+    padding: 2,
+    margin: 3,
+    borderColor: "white",
+    logoColor: "bold-red",
+    textColor: "red",
+  })
+    .emptyLine()
+    .right("version 1.0.0")
+    .emptyLine()
+    .center(longText)
+    .render()
+);
+
+const teamBuilder = new TeamBuilder();
 teamBuilder.start();
